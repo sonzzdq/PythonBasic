6c79cb06f68a7aaac1b0b537f7af0d058fc48479
Truong Son <sonzzdq@outlook.com>
2016-03-29 13:11:53 +0700

    làm quen với pyautogui

---
+++ vdpyautogui.py
@@ -0,0 +1,12 @@
+import pyautogui, time
+time.sleep(3)
+# pyautogui.click()    # click to put drawing program in focus
+# distance = 0
+# while distance < 100:
+#     pyautogui.dragRel(distance, 0, duration=0.2)   # move right
+#     distance = distance + 5
+#     pyautogui.dragRel(0, distance, duration=0.2)   # move down
+#     pyautogui.dragRel(-distance, 0, duration=0.2)  # move left
+#     distance = distance + 5
+#     pyautogui.dragRel(0, -distance, duration=0.2)  # move up
+pyautogui.press('winleft')
